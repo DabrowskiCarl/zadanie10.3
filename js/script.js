f910cd82a51a3864b942f2804688c62aa223e203
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-03-28 23:34:39 +0200

    Fix script look

--- js/script.js
+++ js/script.js
@@ -1,33 +1,17 @@
-/*
-$(function(){
-    const carouselList = $("#carousel ul");
-    const c = setInterval(function(){
-		carouselList.animate({marginLeft:-600},4000,function(){
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-	},5000);
-});
-*/
-
-$(function(){
+$(function () {
 	var carouselList = $('#carousel ul');
 	setInterval(changeSlide, 5000);
-	function changeSlide(){
-		carouselList.animate({'marginLeft':-600}, 600, moveFirstSlide);
+
+	function changeSlide() {
+		carouselList.animate({
+			'marginLeft': -600
+		}, 600, moveFirstSlide);
 	}
-	function moveFirstSlide(){
+
+	function moveFirstSlide() {
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+		carouselList.css({marginLeft: 0});
 	}
-
-
-	
-
-	
-
-
-
 });
